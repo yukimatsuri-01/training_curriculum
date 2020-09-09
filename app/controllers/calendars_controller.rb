9ab44e9300d7bb018165698ad08898476ddd5dee
yukimatsuri-01 <70310758+yukimatsuri-01@users.noreply.github.com>
2020-09-09 17:56:41 +0900

    Update calendars_controller.rb
    大変申し訳ございません。再度修正しましたのでご確認いただけますと幸いです。
    https://gyazo.com/364ed47002d1c3350403604092126c3e

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,11 +34,11 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = 0
-      if wday_num == 7
+      wday_num = wdays[@todays_data.wday]
+      if wday_num.to_i >= 7
         wday_num = wday_num - 7
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: (@todays_date + x).wday}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_data + x).wday.to_i]}
       @week_days.push(days)
     end
 
